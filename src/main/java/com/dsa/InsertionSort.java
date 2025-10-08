ad81609e5fe57826a67a629c0294e843e78f236c
Vinayk Umesh <luxurygeek.umesh@gmail.com>
2025-10-08 08:14:57 +0000

    Added Insertion and Merge sort

---
+++ src/main/java/com/dsa/InsertionSort.java
@@ -0,0 +1,42 @@
+package com.dsa;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        int[] arr = {12, 4, 13, 8, 10, 2, 23};
+
+        insertionSortAsc(arr);
+        System.out.println("Ascending: " + Arrays.toString(arr));
+
+        insertionSortDsc(arr);
+        System.out.println("Descending: " + Arrays.toString(arr));
+    }
+
+    private static void insertionSortDsc(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j] > arr[j - 1]) {
+                    int temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+
+    private static void insertionSortAsc(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+}
